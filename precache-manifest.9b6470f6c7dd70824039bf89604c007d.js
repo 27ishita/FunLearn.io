4b7592f93aed3294bd2f84688d3cbc5b57d894d6
27ishita <49233632+27ishita@users.noreply.github.com>
2020-08-03 19:14:13 +0530

    Updates

---
+++ precache-manifest.9b6470f6c7dd70824039bf89604c007d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2442f8787d7e128e88284edd38face67",
+    "url": "/FunLearn/index.html"
+  },
+  {
+    "revision": "cdff205983573c1a0acb",
+    "url": "/FunLearn/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "175d11b8e6d6e0d296a1",
+    "url": "/FunLearn/static/css/main.12bf6a85.chunk.css"
+  },
+  {
+    "revision": "cdff205983573c1a0acb",
+    "url": "/FunLearn/static/js/2.b75145f9.chunk.js"
+  },
+  {
+    "revision": "406d6b5eeb7d5a908740dc276d0fbc95",
+    "url": "/FunLearn/static/js/2.b75145f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175d11b8e6d6e0d296a1",
+    "url": "/FunLearn/static/js/main.a380b980.chunk.js"
+  },
+  {
+    "revision": "7b50772ea8b9c25f6d38",
+    "url": "/FunLearn/static/js/runtime-main.f42b4d9a.js"
+  },
+  {
+    "revision": "d2c57439205e5f94a04540d3bd1dc94b",
+    "url": "/FunLearn/static/media/about.d2c57439.png"
+  },
+  {
+    "revision": "bb79a099832ae287d3778941b59b5bc5",
+    "url": "/FunLearn/static/media/angular.bb79a099.png"
+  },
+  {
+    "revision": "385b1cd120f9e63862f1c8e8eda080e3",
+    "url": "/FunLearn/static/media/css.385b1cd1.png"
+  },
+  {
+    "revision": "dad093df14897dbfa8f4088da0250337",
+    "url": "/FunLearn/static/media/javascript.dad093df.png"
+  },
+  {
+    "revision": "f34a0218dd2e38726c52ac763df55fa7",
+    "url": "/FunLearn/static/media/node.f34a0218.png"
+  },
+  {
+    "revision": "89f82635530d173c22416e884ea2547a",
+    "url": "/FunLearn/static/media/phaser.89f82635.png"
+  },
+  {
+    "revision": "61e3d5ce3923b1a74eab3434df097990",
+    "url": "/FunLearn/static/media/react.61e3d5ce.png"
+  }
+]);
